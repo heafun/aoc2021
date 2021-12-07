cb831c62bcc8814a83dd368a9e0f7756d5daf515
Marc Rosenberger <marc.rosenberger100@gmail.com>
2021-12-07 12:47:40 +0100

    Add day7

---
+++ day7.py
@@ -0,0 +1,21 @@
+import sys
+
+file = open("inputs/day7.txt")
+line = file.readline().strip().split(",")
+
+
+for sub in range(len(line)):
+    line[sub] = int(line[sub])
+
+maxPos = max(line)
+
+cheapest = sys.maxsize
+
+for i in range(maxPos+1):
+    fuelCost = 0
+    for sub in line:
+        fuelCost += abs(i-sub)
+    if cheapest > fuelCost:
+        cheapest = fuelCost
+
+print(cheapest)
---
+++ day7_2.py
@@ -0,0 +1,31 @@
+import sys
+
+file = open("inputs/day7.txt")
+line = file.readline().strip().split(",")
+
+
+def cost(steps):
+    return sum(range(1, steps+1))
+
+
+for sub in range(len(line)):
+    line[sub] = int(line[sub])
+
+maxPos = max(line)
+
+cheapest = sys.maxsize
+cheapestPos = 0
+avg = int(sum(line) / len(line))
+section = int(len(line) * 0.2)
+
+for i in range(avg - section, avg + section):
+    fuelCost = 0
+    for sub in line:
+        fuelCost += cost(abs(i-sub))
+    if cheapest > fuelCost:
+        cheapest = fuelCost
+        cheapestPos = i
+
+print(cheapest)
+print("at " + str(cheapestPos))
+print("Avg: " + str(avg))
