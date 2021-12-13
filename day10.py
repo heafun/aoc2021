878c3b8d565319b75c4db6f054cb382c49c9bc89
Marc Rosenberger <marc.rosenberger100@gmail.com>
2021-12-13 15:14:59 +0100

    Add day10

---
+++ day10.py
@@ -0,0 +1,35 @@
+def getPoints(character):
+    switcher = {
+        ")": 3,
+        "]": 57,
+        "}": 1197,
+        ">": 25137
+    }
+    return switcher.get(character)
+
+
+file = open("inputs/day10.txt")
+lines = file.readlines()
+
+openList = []
+openings = ["(", "{", "[", "<"]
+closings = [")", "}", "]", ">"]
+
+score = 0
+
+for line in range(len(lines)):
+    lines[line] = lines[line].strip()
+    keep = True
+    for char in lines[line]:
+        if char in openings:
+            openList.append(char)
+        else:
+            lastOpen = openList.pop()
+            if openings.index(lastOpen) != closings.index(char):
+                score += getPoints(char)
+                keep = False
+                break
+    if keep:
+        print(str(line) + ": " + lines[line])
+
+print(score)
---
+++ day10_2.py
@@ -0,0 +1,41 @@
+def getPoints(character):
+    switcher = {
+        "(": 1,
+        "[": 2,
+        "{": 3,
+        "<": 4
+    }
+    return switcher.get(character)
+
+
+file = open("inputs/day10.txt")
+lines = file.readlines()
+
+openings = ["(", "{", "[", "<"]
+closings = [")", "}", "]", ">"]
+
+lineScore = []
+
+for line in range(len(lines)):
+    lines[line] = lines[line].strip()
+    keep = True
+    openList = []
+    for char in lines[line]:
+        if char in openings:
+            openList.append(char)
+        else:
+            lastOpen = openList.pop()
+            if openings.index(lastOpen) != closings.index(char):
+                keep = False
+                break
+    if keep:
+        score = 0
+        while len(openList) != 0:
+            char = openList.pop()
+            score *= 5
+            score += getPoints(char)
+        lineScore.append(score)
+
+lineScore.sort()
+
+print(lineScore[int(len(lineScore) / 2)])
